033e929829577787610f15c7c354da8e345a6e93
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-01-30 18:57:20 +0100

    change

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,6 +1,6 @@
 import Commerce from "@chec/commerce.js";
 
-const checAPIKey = process.env.pk_39183800010b721f6588fc02ec5624ee8898fdad18230;
+const checAPIKey = pk_39183800010b721f6588fc02ec5624ee8898fdad18230;
 
 const commerceConfig = {
   axiosConfig: {
