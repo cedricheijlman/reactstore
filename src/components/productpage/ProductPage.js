38f36abe3ba4178dd5ccc8206908b799291b593e
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-01-31 08:48:19 +0100

    Product Info fetch

--- src/components/productpage/ProductPage.js
+++ src/components/productpage/ProductPage.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import commerce from "../../lib/commerce";
+import "./productpage.css";
 
 function ProductPage() {
-  return <div>ProductPage</div>;
+  const [productInfo, setProductInfo] = useState(null);
+
+  useEffect(() => {
+    const productPath = window.location.pathname.slice(9);
+    commerce.products.retrieve(String(productPath)).then((product) => {
+      setProductInfo(product);
+    });
+  }, []);
+  console.log("Product", productInfo);
+  return (
+    <>
+      {productInfo && (
+        <div>
+          <h1>Text</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ProductPage;
