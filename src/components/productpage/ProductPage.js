330d267b2c59e37ebd055af2cc6b02c4d9abc29f
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-01-31 09:18:36 +0100

    product page card

--- src/components/productpage/ProductPage.js
+++ src/components/productpage/ProductPage.js
@@ -13,13 +13,31 @@ function ProductPage() {
   }, []);
   console.log("Product", productInfo);
   return (
-    <>
+    <div id="productInfo">
       {productInfo && (
-        <div>
-          <h1>Text</h1>
+        <div className="productInfo__card">
+          <img
+            src={
+              productInfo.image !== null
+                ? productInfo.image.url
+                : "../logo192.png"
+            }
+          />
+          <h3>{productInfo.name}</h3>
+          <h6>{productInfo.categories[0] && productInfo.categories[0].name}</h6>
+          {
+            <span
+              className="productInfo__description"
+              dangerouslySetInnerHTML={{ __html: productInfo.description }}
+            />
+          }
+          <div className="productInfo__purchase">
+            <p>{productInfo.price.formatted_with_symbol}</p>
+            <button>Add to Cart</button>
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
